4831f7c32e942762b112811c0676d4b554293509
Vikesh <vikesh.das@subtlelabs.com>
2025-04-04 10:32:27 +0530

    first commit

---
+++ containerize_flask_app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,request,jsonify
+
+app=Flask(__name__)
+
+
+@app.get("/user")
+def get_data():
+    return "fetched user data"
+
+@app.post("/save_user")
+def save_data():
+    data=request.get_json()
+    name=data["name"]
+    phone=data["phone"]
+
+    return jsonify({"name": name, "phone": phone})
+
+@app.post("/user/<int:id>")
+def func(id):
+    print(f"Id is:  {id}")
+    return f"Id is:  {id}"
